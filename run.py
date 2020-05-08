34b85e6f28ec1c84694521874405e380cc72b94e
Alain Kouassi <aokouassi@gmail.com>
2020-05-07 23:36:52 -0500

    First Test successful

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ from spotify_client import SpotifyClient
 
 
 def run():
-    with open('\creds\credentials.json') as data_file:
+    with open('creds\credentials.json') as data_file:
         data = json.load(data_file)
     AUTHTOKEN = data["spotify"]["authToken"]
     spotify_client = SpotifyClient(AUTHTOKEN)
 
     # search for songs
-    artist = input("Input artist name")
-    track = input("Input track name")
+    artist = input("Input artist name:\n")
+    track = input("Input track name:\n")
     spotify_song_id = spotify_client.search_song(artist, track)
     if spotify_song_id:
         added_song = spotify_client.add_song_to_spotify(spotify_song_id)
