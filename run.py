4a2d3d0d9f8ed36664dea9bda69db8105c25e693
Alain Kouassi <aokouassi@gmail.com>
2020-05-07 22:46:45 -0500

    Initial trial code, Basic song addition

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
-# This will be the main running script for the application
+import json
+from spotify_client import SpotifyClient
+
+
+def run():
+    with open('\creds\credentials.json') as data_file:
+        data = json.load(data_file)
+    AUTHTOKEN = data["spotify"]["authToken"]
+    spotify_client = SpotifyClient(AUTHTOKEN)
+
+    # search for songs
+    artist = input("Input artist name")
+    track = input("Input track name")
+    spotify_song_id = spotify_client.search_song(artist, track)
+    if spotify_song_id:
+        added_song = spotify_client.add_song_to_spotify(spotify_song_id)
+        if added_song:
+            print(f"Added {artist}")
+
+
+if __name__ == '__main__':
+    run()
--- spotify_client.py
+++ spotify_client.py
@@ -1,6 +1,40 @@
-import os
+import requests
+import urllib.parse
 
-class Playlist(object):
-    def __init__(self,id,title):
-        self.id = id
-        self.title = title
+class SpotifyClient(object):
+    def __init__(self, api_token):
+        self.api_token = api_token
+
+    def search_song(self, artist, track):
+        query = urllib.parse.quote(f'{artist} {track}')
+        url = f"https://api.spotify.com/v1/search?q={query}&type=track"
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization" : f"Bearer {self.api_token}"
+            }
+        )
+        response_json = response.json()
+        results = response_json['tracks']['items']
+        if results:
+            return results[0]['id']
+        else:
+            raise Exception(f"No song found for {artist} = {track}")
+
+    def add_song_to_spotify(self, song_id):
+        url = "https://api.spotify.com/v1/me/tracks"
+        response = requests.put(
+            url,
+            json={
+                "ids": [song_id]
+            },
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.api_token}"
+            }
+        )
+        return response.ok
+
+    def add_song_to_playlist(self, song_id, playlist_id):
+        pass
