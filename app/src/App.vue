f58a8f04e3acceaffe1b9e43e65e397d84ae43de
Alain Kouassi <aokouassi@gmail.com>
2021-03-11 15:47:07 -0500

    Vuetify slight themeing

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,34 +7,18 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('../src/assets/logo.svg')"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <div class=" height: parent align-center">
+        <h3 class= "display: block; font-weight bold; info--text; text-truncate ">
+          Spotify Playlist Curator
+        </h3>
+      </div>
     </v-app-bar>
 
     <v-main>
--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-    theme: { dark: true },
+const vuetify = new Vuetify({
+    theme: { 
+        themes:{
+            light: {
+                primary: colors.grey.darken2,
+                secondary: colors.grey.darken1,
+                info: colors.grey.lighten1,
+                accent: '#1DB954',
+                background: '#212121'
+            }
+        } 
+    },
 })
+
+export default vuetify
